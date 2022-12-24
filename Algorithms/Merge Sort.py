75d07be0347be707e1d5b2e6434eee8d10a46d9d
tomcheung <thomascheung1007@gmail.com>
2022-12-24 10:41:29 +0000

    test

--- Algorithms/Merge Sort.py
+++ Algorithms/Merge Sort.py
@@ -19,7 +19,7 @@ def mergeSort(arr):
         left = arr[:mid]  # arr = [6, 5, 4]
         right = arr[mid:]  # arr = [8, 1, 9]
 
-        # Sort the two halves
+        # Sort the two halves and recursively divide until base case reached
         mergeSort(left)
         mergeSort(right)
 
@@ -56,7 +56,63 @@ def mergeSort(arr):
             k += 1
 
 
-
 arr = [6, 5, 4, 8, 1, 9]
-mergeSort(arr)
-print(arr)
+
+
+# mergeSort(arr)
+# print(arr)
+
+
+def merge_sort(array):
+    # 1. Store the length of the list
+    list_length = len(array)
+
+    # 2. List with length less than is already sorted
+    if list_length == 1:
+        return array
+
+    # 3. Identify the list midpoint and partition the list into a left_partition and a right_partition
+    mid_point = list_length // 2
+
+    # 4. To ensure all partitions are broken down into their individual components,
+    # the merge_sort function is called and a partitioned portion of the list is passed as a parameter
+    left_partition = merge_sort(array[:mid_point])
+    right_partition = merge_sort(array[mid_point:])
+
+    # 5. The merge_sort function returns a list composed of a sorted left and right partition.
+    return merge(left_partition, right_partition)
+
+
+# 6. takes in two lists and returns a sorted list made up of the content within the two lists
+def merge(left, right):
+    # 7. Initialize an empty list output that will be populated with sorted elements.
+    # Initialize two variables i and j which are used pointers/indexing when iterating through the lists.
+    output = []
+    i = j = 0
+
+    # 8. Executes the while loop if both pointers i and j are less than the length of the left and right lists
+    while i < len(left) and j < len(right):
+        # 9. Compare the elements at every position of both lists during each iteration
+        if left[i] < right[j]:
+            # output is populated with the lesser value
+            output.append(left[i])
+            # 10. Move pointer to the right
+            i += 1
+        else:
+            output.append(right[j])
+            j += 1
+    # 11. The remnant elements are picked from the current pointer value to the end of the respective list
+    output.extend(left[i:])
+    output.extend(right[j:])
+
+    return output
+
+
+def run_merge_sort():
+    unsorted_list = [2, 6, 8, 2, 3, 9, 1, 4, 9]
+    print(unsorted_list)
+    sorted_list = merge_sort(unsorted_list)
+    print(sorted_list)
+
+
+run_merge_sort()
