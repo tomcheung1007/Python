c6876eb761dbfc1be779c6e67232e1a56617a8bf
tomcheung <thomascheung1007@gmail.com>
2023-01-09 21:35:41 +0000

    Update

--- Projects/Rock, Paper Scissors/rps.py
+++ Projects/Rock, Paper Scissors/rps.py
@@ -1,76 +1,128 @@
 from random import randint
 import sys
+import time
 
-game_on = True
-wins = 0
-loss = 0
-ties = 0
+def instructions():
+    # instructions
+    print("ROCK PAPER SCISSORS")
+    print("\nWelcome to Rock, Paper, Scissors. The aim of the game is to beat the computer")
+    time.sleep(4)
+    print("\nThe Rules")
+    time.sleep(1)
+    print("Rock beats Scissors")
+    time.sleep(2)
+    print("Scissors beats Paper")
+    time.sleep(2)
+    print("Paper beats Rock")
+    time.sleep(3)
+    print("\nHow to play: Enter R for Rock. Enter P for Paper. Enter S for Scissors")
+    print("Enter Q anytime to quit\n")
 
-#instructions
-print("ROCK PAPER SCISSORS")
-print("\nWelcome to Rock, Paper, Scissors. The aim of the game is to beat the computer")
-print("\nThe Rules")
-print("Rock beats Scissors")
-print("Scissors beats Paper")
-print("Paper beats Rock")
-print("\nEnter R for Rock. Enter P for Paper. Enter S for Scissors")
-print("Enter Q to quit\n")
 
-#player input
-while game_on:
-    print("Wins", wins,"\nTies",ties,"\nLoss",loss)
-    choice = input("\nRock! Paper! Scissors!:\t")
-    if choice.lower() == "r":
-        print("ROCK versus...")
-    elif choice.lower() == "p":
-        print("PAPER versus...")
-    elif choice.lower() == "s":
-        print("SCISSORS versus...")
-    elif choice.lower() == "q":
-        sys.exit()
+def game():
+    game_on = True
+    wins = 0
+    loss = 0
+    ties = 0
 
-#computer input
-    random_number = randint(1,3)
-    if random_number == 1:
-        computer_move = "p"
-        print("PAPER")
-    elif random_number == 2:
-        computer_move = "r"
-        print("ROCK")
-    elif random_number == 3:
-        computer_move = "s"
-        print("SCISSORS")
+    # player input
+    while game_on:
+        print("Wins", wins, "\nTies", ties, "\nLoss", loss)
+        time.sleep(2)
+        choice = input("\nRock! Paper! Scissors!:\t")
+        if choice.lower() == "r":
+            time.sleep(0.5)
+            print("ROCK versus...")
+        elif choice.lower() == "p":
+            time.sleep(0.5)
+            print("PAPER versus...")
+        elif choice.lower() == "s":
+            time.sleep(0.5)
+            print("SCISSORS versus...")
+        elif choice.lower() == "q":
+            time.sleep(0.5)
+            print("\nGame ended!!")
+            time.sleep(0.5)
+            print("Wins", wins, "\nTies", ties, "\nLoss", loss)
+            sys.exit()
 
-#player - computer comparison
+        # computer input
+        random_number = randint(1, 3)
+        if random_number == 1:
+            computer_move = "p"
+            time.sleep(0.5)
+            print("PAPER")
+        elif random_number == 2:
+            computer_move = "r"
+            time.sleep(0.5)
+            print("ROCK")
+        elif random_number == 3:
+            computer_move = "s"
+            time.sleep(0.5)
+            print("SCISSORS")
 
-    if choice == "r" and computer_move == "p":
-        print("\nROCK WINS")
-        wins += 1
-    elif choice == "r" and computer_move == "s":
-        print("\nSCISSORS WINS")
-        loss += 1
-    elif choice == "r" and computer_move == "r":
-        print("\nDRAW")
-        ties += 1
-    elif choice == "p" and computer_move == "r":
-        print("\nPAPER WINS")
-        wins += 1
-    elif choice == "p" and computer_move == "s":
-        print("\nSCISSORS WINS")
-        loss += 1
-    elif choice == "p" and computer_move == "p":
-        print("DRAW")
-        ties += 1
-    elif choice == "s" and computer_move == "p":
-        print("\nSCISSORS WINS")
-        wins += 1
-    elif choice == "s" and computer_move == "s":
-        print("DRAW")
-        ties += 1
-    elif choice == "s" and computer_move == "r":
-        print("\nROCK WINS")
-        loss += 1
-    
+        # result
+        if choice == "r" and computer_move == "p":
+            time.sleep(0.5)
+            print("\nROCK WINS")
+            wins += 1
+        elif choice == "r" and computer_move == "s":
+            time.sleep(0.5)
+            print("\nSCISSORS WINS")
+            loss += 1
+        elif choice == "r" and computer_move == "r":
+            time.sleep(0.5)
+            print("\nDRAW")
+            ties += 1
+        elif choice == "p" and computer_move == "r":
+            time.sleep(0.5)
+            print("\nPAPER WINS")
+            wins += 1
+        elif choice == "p" and computer_move == "s":
+            time.sleep(0.5)
+            print("\nSCISSORS WINS")
+            loss += 1
+        elif choice == "p" and computer_move == "p":
+            time.sleep(0.5)
+            print("\nDRAW")
+            ties += 1
+        elif choice == "s" and computer_move == "p":
+            time.sleep(0.5)
+            print("\nSCISSORS WINS")
+            wins += 1
+        elif choice == "s" and computer_move == "s":
+            time.sleep(0.5)
+            print("\nDRAW")
+            ties += 1
+        elif choice == "s" and computer_move == "r":
+            time.sleep(0.5)
+            print("\nROCK WINS")
+            loss += 1
 
+
+# LOGIC
+def play_rps():
+    # instructions()
+    while True:
+        play = " "
+        game_on = False
+        while play not in ["Y", "Q"]:
+            time.sleep(2)
+            play = input("Are you ready to play? Enter Y (yes) or Q (quit):\t").upper()
+
+            if play == "Y":
+                game_on = True
+            elif play == "Q":
+                game_on = False
+            else:
+                print("Error. Please enter Y to play or Q to quit")
+
+            while game_on:
+                game()
+
+        print("ASD")
+
+
+print(play_rps())
 
 
---
+++ Projects/Udemy Projects/Credit Card Validator.py
@@ -0,0 +1,35 @@
+def card_info():
+
+    while True:
+        card_number = input("Please enter 16 digit card number:\t")
+        if len(card_number) != 16:
+            print("*ERROR* - Card number must be 16 digits long")
+        else:
+            if card_number.isdigit() is False:
+                print("*ERROR* - Card number must be numbers only")
+            else:
+                print("Thank you - Card number accepted ")
+                return card_number
+
+
+def validate_card(card):
+    final = [int(_) for i, _ in enumerate(card) if i % 2 == 1]
+# 1. Starting from index 0, double every other digit.
+    double_card_num = [int(_) * 2 for i, _ in enumerate(card) if i % 2 == 0]
+# 2. If element is two-digits, replace element with sum of two digits together. Add to final
+    two_digit = [str(_) for _ in double_card_num if len(str(_)) == 2]  # list of numbers two digit long
+    for _ in two_digit:
+        total = int(_[0]) + int(_[1])
+        final.append(total)
+# 3. Remaining single digit elements added to final
+    remaining = [str(_) for _ in double_card_num if len(str(_)) == 1]
+    for _ in remaining:
+        final.append(int(_))
+# 4. if sum(final) mod 10 equal to 0: Valid according to Luhn formula
+    total_sum = sum(final)
+    if total_sum % 10 == 0:
+        return "Valid card"
+    else:
+        return "Invalid card"
+
+print(validate_card(card_info()))
